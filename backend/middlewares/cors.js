6637c8f2df0adde5a0cdae79580c96a6df148615
Natalya Olimpieva <ukurennyi.koshak@gmail.com>
2021-12-08 15:47:36 +0300

    refactor: cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,25 +1,40 @@
-const { allowedCors, DEFAULT_ALLOWED_METHODS } = require('../utils/constants');
+// const { allowedCors, DEFAULT_ALLOWED_METHODS } = require('../utils/constants');
+
+const allowedCors = [
+  'https://mesto-project.nomoredomains.rocks',
+  'http://mesto-project.nomoredomains.rocks',
+  'http://localhost:3000',
+];
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
 
+  // eslint-disable-next-line
   console.log({ origin });
-  console.log(allowedCors.includes(origin));
+  // eslint-disable-next-line
+  console.log({ 'isAllowedCorsIncludesOrigin': allowedCors.includes(origin) });
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Credentials', true);
+    // eslint-disable-next-line
+    console.log({ 'ResHeader': res.header })
   }
 
   const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
+  // eslint-disable-next-line
   console.log({ requestHeaders });
+  // eslint-disable-next-line
   console.log({ method });
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
+    // eslint-disable-next-line
+    console.log({ 'ResHeader': res.header })
     res.end();
   }
 
--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -2,17 +2,17 @@ const OK = 200;
 
 const urlServer = 'mongodb://localhost:27017/mestodb';
 
-const allowedCors = [
-  'https://mesto-project.nomoredomains.rocks',
-  'http://mesto-project.nomoredomains.rocks',
-  'localhost:3000',
-  'http://localhost:3000',
-];
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+// const allowedCors = [
+//   'https://mesto-project.nomoredomains.rocks',
+//   'http://mesto-project.nomoredomains.rocks',
+//   'localhost:3000',
+//   'http://localhost:3000',
+// ];
+// const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports = {
   OK,
   urlServer,
-  allowedCors,
-  DEFAULT_ALLOWED_METHODS,
+  // allowedCors,
+  // DEFAULT_ALLOWED_METHODS,
 };
