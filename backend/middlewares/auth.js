7a3c3447c3c4c7dcea1c4784b65ba19a65a13b37
Natalya Olimpieva <ukurennyi.koshak@gmail.com>
2021-12-08 17:17:24 +0300

    fix: error http headers sent in auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,16 +5,19 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
+
   if (token) {
     let payload;
     try {
       payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret');
     } catch (error) {
-      next(new UnauthorizedError('Переданы некорректные данные при авторизации пользователя.'));
+      return next(new UnauthorizedError('Переданы некорректные данные при авторизации пользователя.'));
     }
     req.user = payload;
     next();
   } else {
     next(new UnauthorizedError('Необходима авторизация.'));
   }
+
+  return null;
 };
